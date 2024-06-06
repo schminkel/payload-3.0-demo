5ce99665b10868e99564e5c65b44e1a4cbe67b7b
THORSTEN SCHMINKEL <thorsten.a.schminkel@capgemini.com>
2024-06-06 14:09:37 +0200

    change env to POSTGRES_URL

--- payload.config.ts
+++ payload.config.ts
@@ -123,7 +123,7 @@ export default buildConfig({
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.POSTGRES_URI || '',
+      connectionString: process.env.POSTGRES_URL || '',
     },
   }),
   // db: mongooseAdapter({
